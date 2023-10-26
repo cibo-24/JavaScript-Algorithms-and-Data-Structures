21a91a2151cd4862b4e27fb7da6f50618541ce3f
cibo-24 <cibo.kenan@gmail.com>
2023-10-26 22:32:10 +0300

    create promise

---
+++ EcmaScript/Promise/promise.js
@@ -0,0 +1,8 @@
+// promise bir kurucu functiondur.
+// bir tane oluşturmak için new anahtar sözcüğü kullanmak gerektir.
+// argüman olarak resolve ve reject olarak 2 parametre alır.
+// vaat doğruysa resolve değilse reject dönecektir.
+
+const makeServerRequest = new Promise ((resolve,reject) => {
+
+});
