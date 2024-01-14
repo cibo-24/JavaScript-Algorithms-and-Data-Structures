96955945b8a93ff2114cfb1dcea2eb3110ea00e5
cibo-24 <cibo.kenan@gmail.com>
2024-01-14 19:43:32 +0300

    Functional Programming

---
+++ 08-Functional-Programming/05-Dışa Bağımlılığı Önlemek - argüman/func.js
@@ -0,0 +1,12 @@
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(value) {
+  return value + 1;
+}
+// Only change code above this line
+
+// Test the function
+let result = incrementer(fixedValue);
+console.log(result); // Output: 5
